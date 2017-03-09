e3baabd631022cce1daee66a5ba99acad62c9478
Isangeles <darekapo1@gmail.com>
2017-03-09 01:04:34 +0100

    From linux

---
+++ src/pl/isangeles/senlin/graphic/Avatar.java
@@ -0,0 +1,13 @@
+package pl.isangeles.senlin.graphic;
+
+import org.newdawn.slick.SpriteSheet;
+
+public class Avatar 
+{
+	SpriteSheet basicTorso;
+	
+	public Avatar()
+	{
+		//basicTorso = new SpriteSheet("");
+	}
+}
--- src/pl/isangeles/senlin/states/GameWorld.java
+++ src/pl/isangeles/senlin/states/GameWorld.java
@@ -1,36 +1,37 @@
 package pl.isangeles.senlin.states;
 
+import java.io.File;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class GameWorld extends BasicGameState
 {
-
+	TiledMap areaMap;
+	
     @Override
     public void init(GameContainer container, StateBasedGame game)
             throws SlickException
     {
-        // TODO Auto-generated method stub
-
+    	areaMap = new TiledMap(new String("data" + File.separator + "area" + File.separator + "map" + File.separator + "area01.tmx"));
+    	
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g)
             throws SlickException
     {
-        // TODO Auto-generated method stub
-
+    	areaMap.render(0, 0);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta)
             throws SlickException
     {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
