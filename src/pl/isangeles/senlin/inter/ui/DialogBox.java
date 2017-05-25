d598053973b465985062c03845d6e259f2f709e4
Isangeles <dev@isangeles.pl>
2017-05-25 18:47:03 +0200

    Improvements for dialog box

--- src/pl/isangeles/senlin/inter/ui/DialogBox.java
+++ src/pl/isangeles/senlin/inter/ui/DialogBox.java
@@ -29,7 +29,7 @@ class DialogBox extends InterfaceObject implements UiElement
 	private Character interlocutorA;
 	private Character interlocutorB;
 
-	private List<String> dialogueBoxContent = new ArrayList<>();
+	private List<DialogueText> dialogueBoxContent = new ArrayList<>();
 	private List<String> dialogueBoxTextA = new ArrayList<>();
 	private List<String> dialogueBoxTextB = new ArrayList<>();
 	private List<Answer> dialogueAnswers;
@@ -38,6 +38,8 @@ class DialogBox extends InterfaceObject implements UiElement
 	private TrueTypeFont ttf;
 	
 	private boolean openReq;
+	
+	private GameContainer gc;
 	/**
 	 * Dialogue box constructor
 	 * @param gc Slick game container
@@ -73,9 +75,9 @@ class DialogBox extends InterfaceObject implements UiElement
 		for(int i = 0; i < dialogueBoxContent.size(); i ++)
 		{
 			if(dialogueBoxTextB.contains(dialogueBoxContent.get(i)))
-				ttf.drawString(x+getDis(260), (y+getDis(300)) - (ttf.getHeight(dialogueBoxContent.get(i))*i), dialogueBoxContent.get((dialogueBoxContent.size()-1)-i));
+				dialogueBoxContent.get((dialogueBoxContent.size()-1)-i).draw(x+getDis(260), (y+getDis(300)) - (ttf.getHeight(dialogueBoxContent.get(i).getText())*i));
 			if(dialogueBoxTextA.contains(dialogueBoxContent.get(i)))
-				ttf.drawString(x+getDis(15), (y+getDis(300)) - (ttf.getHeight(dialogueBoxContent.get(i))*i), dialogueBoxContent.get((dialogueBoxContent.size()-1)-i));
+				dialogueBoxContent.get((dialogueBoxContent.size()-1)-i).draw(x+getDis(15), (y+getDis(300)) - (ttf.getHeight(dialogueBoxContent.get(i).getText())*i));
 		}
 		
 		for(int i = 0; i < options.size(); i ++)
@@ -118,7 +120,14 @@ class DialogBox extends InterfaceObject implements UiElement
 			this.interlocutorB = interlocutorB;
 
 			dialogueBoxTextB.add(interlocutorB.getDialog().getText());
-			dialogueBoxContent.add(interlocutorB.getDialog().getText());
+			try 
+			{
+				dialogueBoxContent.add(new DialogueText(interlocutorB.getDialog().getText(), gc));
+			} 
+			catch (SlickException | IOException e) 
+			{
+				Log.addSystem("dialogbox_text_fail msg///" + e.getMessage());
+			}
 			dialogueAnswers = interlocutorB.getDialog().getAnswers();
 			addOptions(dialogueAnswers);
 			openReq = true;
@@ -160,7 +169,14 @@ class DialogBox extends InterfaceObject implements UiElement
         interlocutorB.getDialog().answerOn(dialogueOption);
         clearAnswersBox();
         dialogueBoxTextB.add(interlocutorB.getDialog().getText());
-		dialogueBoxContent.add(interlocutorB.getDialog().getText());
+		try 
+		{
+			dialogueBoxContent.add(new DialogueText(interlocutorB.getDialog().getText(), gc));
+		} 
+		catch (SlickException | IOException e) 
+		{
+			Log.addSystem("dialogbox_text_fail msg///" + e.getMessage());
+		}
         dialogueAnswers = interlocutorB.getDialog().getAnswers();
         addOptions(dialogueAnswers);
 	}
@@ -208,7 +224,14 @@ class DialogBox extends InterfaceObject implements UiElement
 				else if(option != null && !option.isEnd())
 				{
 					dialogueBoxTextA.add(option.getText());
-					dialogueBoxContent.add(option.getText());
+					try 
+					{
+						dialogueBoxContent.add(new DialogueText(option.getText(), gc));
+					} 
+					catch (SlickException | IOException e) 
+					{
+						Log.addSystem("dialogbox_text_fail msg///" + e.getMessage());
+					}
 				    nextDialogueStage(option);
 				}
 			}
@@ -230,13 +253,48 @@ class DialogBox extends InterfaceObject implements UiElement
 		}
 	}
 	
-	private class DialogueText extends InterfaceObject
+	private class DialogueText
 	{
+		private List<String> textLines = new ArrayList<>();
+		private String text;
 		public DialogueText(String text, GameContainer gc) throws SlickException, IOException
 		{
-			super(GConnector.getInput("field/infoWindowBG.png"), "uiDialogueBoxBG", false, gc);
+			this.text = text;
+			/*
+			for(int i = 0, j = 0, k = 0; i < text.length(); i ++, j ++)
+			{
+				if(i == 0)
+				{
+					textLines.add(text.substring(0, 16));
+				}
+				if(j >= 15)
+				{
+					textLines.add(text.substring(k, j));
+					k = j;
+					j = 0;
+				}
+			}
+			*/
 		}
 		
+		public void draw(float x, float y)
+		{
+			ttf.drawString(x, y, text);
+			/*
+			for(int i = 0; i < textLines.size(); i ++)
+			{
+		        if(textLines.size() > 1)
+		            ttf.drawString(x+getDis(10), y+ttf.getHeight(textLines.get(i))*i, textLines.get(i));
+		        else
+		            ttf.drawString(x+getDis(10), y, textLines.get(i));
+			}
+			*/
+		}
 		
+		public String getText()
+		{
+			return text;
+		}
+	
 	}
 }
