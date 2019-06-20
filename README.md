43df364635122b1344b9d0b9653448a3d170fe52
Isangeles <darekapo1@gmail.com>
2019-06-20 16:43:26 +0200

    support for gradle build, readme update

--- README.md
+++ README.md
@@ -1,32 +1,44 @@
 # Senlin
-
-Senlin is completly free and open source 2D cRPG game. Game works on its own engine written from scratch in Java with Slick2D library. 
-Game engine is completly modular, it means you can easly creats your own modules with maps, NPCs, items, quests and dialogues without any source code modification. 
-Engine does not use any binary files, so all data files like items/quests/NPCs bases, scenarios, saved games, are easy to access and modifiy, most of them are in XML format.
+Senlin is completely free and open source 2D cRPG game. Game works on its own engine written from scratch in Java with Slick2D library.
+Game engine is completely modular, it means you can easily create your own modules with maps, NPCs, items, quests and dialogues without any source code modification.
+Engine does not use any binary files, so all data files like item bases, scenarios, saved games, are easy to read and modify, most of them are in XML format.
 Currently available in alpha version.
 
-## Installation
+For more information/help check [game documentation](http://senlin.isangeles.pl/guide/).
 
+## Installation
 Simply [download](https://github.com/Isangeles/Senlin/releases) and unpack build.
 
 ## Run
-
 Simply run senlin executable(.sh on Linux or .exe on Windows).
 
 If you have problem with game startup, try to change game resolution(in game.conf) to your current resolution or one of these: 1920x1080, 1600x800, 1280x720.
 
+## Build
+You can easily build project using [Gradle](https://gradle.org/).
+
+Navigate to main directory and run:
+```
+  gradle run
+```
+or to get executable files:
+```
+  gradle build
+```
+
 ## Screenshots
-![sc1](/screenshots/sc2.jpg)
-![sc2](/screenshots/sc3.jpg)
-![sc3](/screenshots/sc4.jpg)
+![sc1](screenshots/sc2.jpg)
+![sc2](screenshots/sc3.jpg)
+![sc3](screenshots/sc4.jpg)
+
+## Contact
+* Isangeles <<dev@isangeles.pl>>
 
 ## License
+Copyright 2017-2019 Dariusz Sikora <<dev@isangeles.pl>>
 
-Copyright 2018 Dariusz Sikora
-
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. 
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
---
+++ src/main/java/pl/isangeles/senlin/SenlinGame.java
@@ -0,0 +1,94 @@
+/*
+ * SenlinGame.java
+ *
+ * Copyright 2017-2019 Dariusz Sikora <dev@isangeles.pl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+package pl.isangeles.senlin;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.File;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.ScalableGame;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import pl.isangeles.senlin.states.*;
+import pl.isangeles.senlin.util.Settings;
+import pl.isangeles.senlin.util.Size;
+
+/**
+ * Main game class, contains all game states
+ * @author Isangeles
+ *
+ */
+public class SenlinGame extends StateBasedGame
+{
+	public static final String VERSION = "0.1.3";
+
+    public SenlinGame(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException
+    {
+        this.addState(new MainMenu());
+        this.addState(new NewGameMenu());
+        this.addState(new SettingsMenu());
+        this.addState(new LoadMenu());
+        this.enterState(0);
+    }
+
+    public static void main(String[] args)
+    {
+				// Set path to native lwjgl libs.
+				System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
+				// Create game container.
+        try
+        {
+        	AppGameContainer appGC = new AppGameContainer(new ScalableGame(new SenlinGame("Senlin " + VERSION), (int)Settings.getResolution()[0], (int)Settings.getResolution()[1]));
+        	try
+            {
+            	appGC.setDisplayMode((int)Settings.getResolution()[0], (int)Settings.getResolution()[1], Settings.isFullscreen());
+            }
+            catch(SlickException e)
+            {
+            	System.err.println("engine_msg: Unsupported resolution: " + Settings.getResolution()[0] + "x" + Settings.getResolution()[1] + ", switching to system resolution...");
+            	Settings.setResolution(Settings.getSystemResolution());
+            	System.gc();
+            	appGC = new AppGameContainer(new ScalableGame(new SenlinGame("Senlin " + VERSION), (int)Settings.getResolution()[0], (int)Settings.getResolution()[1]));
+            	appGC.setDisplayMode((int)Settings.getResolution()[0], (int)Settings.getResolution()[1], false);
+            }
+            appGC.setTargetFrameRate(60);
+            //appGC.setClearEachFrame(false);
+            appGC.setIcon("icon.png");
+            appGC.start();
+        }
+        catch(SlickException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+}
